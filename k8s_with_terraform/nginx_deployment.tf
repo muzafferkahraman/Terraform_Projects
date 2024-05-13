655618defc1d6e985e05f56427a8161927858afc
Muzaffer Kahraman <muzaffer.kahraman@outlook.com>
2024-05-13 17:35:06 +0300

    Update and rename kubernetes.tf to nginx_deployment.tf

--- k8s_with_terraform/nginx_deployment.tf
+++ k8s_with_terraform/nginx_deployment.tf
@@ -12,19 +12,11 @@ variable "cluster_ca_certificate" {
 
 }
 
-
-
-  
-
-
 provider "kubernetes" {  
   host = var.host  
-  
   client_certificate     = base64decode(var.client_certificate)
   client_key             = base64decode(var.client_key)
   cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
-
-
 }
 
 resource "kubernetes_deployment" "nginx" {
