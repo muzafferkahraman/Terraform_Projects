522da5b610a176b1f439e27b4658718660eea2fc
root <root@localhost.localdomain>
2024-05-29 11:42:31 +0300

    -

--- aws_with_terraform/basic_ec2/provision_ec2.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_instance" "ec2" { // internal names used by Terraform
-ami = "ami-0d3a2960fcac852bc" // that id belongs tothe  region eu-north-1
-instance_type = "t3.micro"
-tags = {
-Name = "muzo" // that's the name of the EC2 instance
-}
-}
